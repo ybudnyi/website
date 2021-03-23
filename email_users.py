40e30057c3e6d0027a57eea7911d32d76494f09c
Yurii Budnyi <ybudnyi@gmail.com>
2021-03-23 17:20:55 +0200

    mass_index_webpage

--- email_users.py
+++ email_users.py
@@ -2,15 +2,24 @@ from email.mime.text import MIMEText
 import smtplib
 
 
-def send_letter(email):
-    from_email = ''
-    from_password = ''
+def send_letter(email, mass, average, users):
+    from_email = 'budnyy5@gmail.com'
+    from_password = 'Uralviv1987'
     to_email = email
 
-    subject = ''
-
-    message =''
-
+    subject = 'Body mass index'
+    if int(mass) > 25:
+        message = f'Hi. Body mass index: <strong>{mass}</strong>,  among {users} users.' \
+                  f' Your body mass index is <strong>higher</strong> of normal. You should do some exercises. ' \
+                  f'Average mass index of our users is: {average}.'
+    elif int(mass) < 18:
+        message = f'Hi. Body mass index: <strong>{mass}</strong>,  among {users} users.' \
+                  f'Your body max index is <strong>lover</strong> of normal. You should eat more meat. ' \
+                  f'Average mass index of our users is: {average}'
+    else:
+        message = f'Hi. Body mass index: <strong>{mass}</strong>,  among {users} users.' \
+                  f'Your body max index is <strong>normal</strong>. You are doing great.' \
+                  f'Average mass index of our users is: {average}'
     msg = MIMEText(message, 'html')
     msg['Subject'] = subject
     msg['To'] = to_email
--- main.py
+++ main.py
@@ -1,70 +1,59 @@
-from flask import Flask, render_template, request, redirect
-import csv
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
-# from email_users import send_letter
-
+from sqlalchemy.sql import func
+from email_users import send_letter
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:3337@localhost/database_name'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:3337@localhost/w_index'
 db = SQLAlchemy(app)
 
 
 class MyDB(db.Model):
-    __tablename__ = 'table_name'
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), nullable=False)
-    subject = db.Column(db.String(200), nullable=False)
+    email_ = db.Column(db.String(120), unique=True, nullable=False)
+    weight_ = db.Column(db.Integer, nullable=False)
+    height_ = db.Column(db.Integer, nullable=False)
+    index_ = db.Column(db.Integer, nullable=False)
 
-
-    def __init__(self, email, subject):
-        self.email = email
-        self.subject = subject
+    def __init__(self, email, weight, height, index):
+        self.email_ = email
+        self.weight_ = weight
+        self.height_ = height
+        self.index_ = index
 
 
 db.create_all()
 
 
-
-
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
 
-@app.route('/<string:page_name>')
-def page_name(page_name):
-    return render_template(page_name)
-
-
-@app.route("/submit_form", methods=['POST', 'GET'])
-def submit_form():
+@app.route("/info", methods=['POST'])
+def info():
     if request.method == 'POST':
-        data = request.form.to_dict()
-        write_to_csv(data)
-        print('after csv')
-        email = request.form['email']
-        subject = request.form['subject']
-        db_data = MyDB(email, subject)
-        db.session.add(db_data)
-        db.session.commit()
-        # send_letter()
-        print('add to db')
-        return redirect('/thanks.html')
+        email = request.form['email_addr']
+        weight = request.form['weight']
+        height = request.form['height']
+        w_index = int(int(weight) / ((int(height) / 100) ** 2))
+        if db.session.query(MyDB).filter(MyDB.email_ == email).count() == 0:
+            db_data = MyDB(email, weight, height, w_index)
+            db.session.add(db_data)
+            db.session.commit()
+            avr_index = db.session.query(func.avg(MyDB.index_)).scalar()
+            avr_index = int(avr_index)
+            count = db.session.query(MyDB.email_).count()
+            send_letter(email, w_index, avr_index, count)
+            print(avr_index)
+        else:
+            return render_template('index.html', text='This email is already in use!')
+        return render_template('info.html')
 
-
-
-def write_to_csv(mess):
-    with open('database.csv', mode='a') as database:
-        fieldnames = ['email', 'subject', 'text_message']
-        writer = csv.DictWriter(database, fieldnames=fieldnames)
-        writer.writeheader()
-        writer.writerow(mess)
-        print('add to csv')
-
-
-count = db.session.query(MyDB.email).count()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.debug = True
+    app.run()
